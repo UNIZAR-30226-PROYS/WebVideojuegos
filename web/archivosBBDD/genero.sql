50b099b52c04e24f18b5c98bd22f62484b839d81
alfondel <alfondel95@gmail.com>
2017-04-02 18:21:12 +0200

    Add files via upload

---
+++ web/archivosBBDD/genero.sql
@@ -0,0 +1,5 @@
+insert into genero (id, nombre) values (1, 'Acción');
+insert into genero (id, nombre) values (2, 'Estrategia');
+insert into genero (id, nombre) values (3, 'Rol');
+insert into genero (id, nombre) values (4, 'Deportes');
+insert into genero (id, nombre) values (5, 'Conducción');
