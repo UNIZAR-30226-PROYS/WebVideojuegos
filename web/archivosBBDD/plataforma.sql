50dc5be25871686f9714c22803d297bdcc231a2a
alfondel <alfondel95@gmail.com>
2017-04-02 18:23:33 +0200

    Update plataforma.sql

--- web/archivosBBDD/plataforma.sql
+++ web/archivosBBDD/plataforma.sql
@@ -1,5 +1,5 @@
-insert into plataforma (id, nombre) values (1, "PC");
-insert into plataforma (id, nombre) values (2, "XBOX1");
-insert into plataforma (id, nombre) values (3, "PS4");
-insert into plataforma (id, nombre) values (4, "WIIU");
-insert into plataforma (id, nombre) values (5, "Switch");
+insert into plataforma (id, nombre) values (1, 'PC');
+insert into plataforma (id, nombre) values (2, 'XBOX1');
+insert into plataforma (id, nombre) values (3, 'PS4');
+insert into plataforma (id, nombre) values (4, 'WIIU');
+insert into plataforma (id, nombre) values (5, 'Switch');
