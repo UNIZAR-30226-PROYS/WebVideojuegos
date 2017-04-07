49edfed0a704dc69038f7a8f4c87b38f0ca91036
alfondel <alfondel95@gmail.com>
2017-04-07 16:13:09 +0200

    Add files via upload

---
+++ web/archivosBBDD/desarrolladora.sql
@@ -0,0 +1,5 @@
+insert into desarrolladora (id, nombre ) values (1, 'Activision');
+insert into desarrolladora (id, nombre ) values (2, 'Buggisoft');
+insert into desarrolladora (id, nombre ) values (3, 'Blizzard');
+insert into desarrolladora (id, nombre ) values (4, 'FromSoftware');
+insert into desarrolladora (id, nombre ) values (5, 'Riot');
