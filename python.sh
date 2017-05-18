1d52fa6d7a84ed3912576f3afb47d705b2192185
Bea95 <beamarand95@gmail.com>
2017-05-18 21:14:49 +0200

    python

--- python.sh
+++ python.sh
@@ -1,5 +1,10 @@
 #Si no funciona pip actualizamos python
+su
 wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
-tar xvf Python-3.6.0.tgz
+tar -xvf Python-3.6.0.tgz
+cd Python-3.6.0
+./configure
+make -j8
 make altinstall
+python3.6
 
