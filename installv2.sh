ec53043b79e3c60e87b7e69bc119dcab78f03da6
Bea95 <beamarand95@gmail.com>
2017-05-19 02:48:15 +0200

    scripts

--- installv2.sh
+++ installv2.sh
@@ -1,20 +1,27 @@
-#instalamos si no lo esta git
+instalamos git
 dpkg -l | grep -qw git || apt-get install git
 
 #clonamos el repositiorio
 git clone https://github.com/UNIZAR-30226-2017-05/WebVideojuegos.git
 
+#instalamos y/o actualizamos pip
+dpkg -l | grep -qw python-pip || sudo apt-get install python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+
 #instalamos FLASK
-chmod +x WebVideojuegos/web/install.sh
+sudo chmod 0777 WebVideojuegos/web/install.sh
 ./WebVideojuegos/web/install.sh
 
 #instalamos si no lo esta mysql
-dpkg -l | grep -qw mysql-server || apt-get install mysql-server
-dpkg -l | grep -qw python-mysqldb || apt-get install python-mysqldb
+dpkg -l | grep -qw mysql-server || sudo apt-get install mysql-server
+dpkg -l | grep -qw python-mysqldb || sudo apt-get install python-mysqldb
+
 service mysql start
 
 #creamos y poblamos la base de datos
-mysql -u root -p 
-source ./WebVideojuegos/web/archivosBBDD/crearBBDD.sql
-source ./WebVideojuegos/web/archivosBBDD/crear.sql
-exit
+echo "Cuando pida la contraseña, introducir la contraseña de mysql de root"
+cd ./WebVideojuegos/web/archivosBBDD/
+mysql -u root -p < crearBBDD.sql
+mysql -u root -p < crear.sql
+quit
--- python.sh
+++ python.sh
@@ -1,10 +1,11 @@
 #Si no funciona pip actualizamos python
-su
 wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
+sudo chmod 0777 Python-3.6.0.tgz
 tar -xvf Python-3.6.0.tgz
+sudo chmod 0777 Python-3.6.0
 cd Python-3.6.0
-./configure
+./configure --enable-optimizations
 make -j8
 make altinstall
-python3.6
-
+sudo apt-get update
+sudo apt-get upgrade python
