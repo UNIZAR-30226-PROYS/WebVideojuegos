eb71a9d75d2bf6ce3e84ad63643557505730730c
Macosgra <macosgra@hotmail.com>
2017-05-25 12:45:42 +0200

    escript de instalacion corregido

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
+#instalamos git
+dpkg -L git >/dev/null 2&>1 || sudo apt-get install -y git
 
 #clonamos el repositiorio
 git clone https://github.com/UNIZAR-30226-2017-05/WebVideojuegos.git
 
+#instalamos y/o actualizamos pip
+dpkg -L python-pip>/dev/null 2&>1 || sudo apt-get install -y python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+
 #instalamos FLASK
-chmod +x WebVideojuegos/web/intall.sh
-./WebVideojuegos/web/intall.sh
+chmod 0777 WebVideojuegos/web/install.sh
+sudo ./WebVideojuegos/web/install.sh
 
 #instalamos si no lo esta mysql
-dpkg -l | grep -qw mysql-server || apt-get install mysql-server
-dpkg -l | grep -qw MySQL-python || apt-get install MySQL-python
-service mysql start
+dpkg -L mysql-server || sudo apt-get install mysql-server
+dpkg -L python-mysqldb || sudo apt-get install python-mysqldb
+
+sudo service mysql start
 
 #creamos y poblamos la base de datos
-echo "Si solicita contraseña pulsar enter sin escribir nada"
-mysql -u root -p -e "source ./WebVideojuegos/web/archivosBBDD/crearBBDD.sql"
-mysql -u software -p'software' 'proySoftware' -e "source ./WebVideojuegos/web/archivosBBDD/crearBBDD.sql"
+echo "Cuando pida la contraseña, introducir la contraseña de mysql de root"
+cd ./WebVideojuegos/web/archivosBBDD/
+mysql -u root -p < crearBBDD.sql
+mysql -u root -p < crear.sql
 
-#En construccion
